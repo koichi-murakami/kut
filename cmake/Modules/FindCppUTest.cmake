e9d611d85fd0be043da93815aebadcdbb50be7e7
Koichi Murakami <koichi.murakami@kek.jp>
2015-12-03 15:02:15 +0900

    quiet for pkg_config messages

--- cmake/Modules/FindCppUTest.cmake
+++ cmake/Modules/FindCppUTest.cmake
@@ -1,19 +1,25 @@
 # - Find CPPUTest library
+#
 # It defines:
 # CPPUTEST_FOUND               If the CPPUTest is found
 # CPPUTEST_INCLUDE_DIRS        PATH to the include directory
 # CPPUTEST_LIBRARIES           libraries
+#
+# CPPUTEST_DIR is used for guessing the installed path.
+#
 
-find_package(PkgConfig)
+find_package(PkgConfig QUIET)
 
-pkg_check_modules(PC_CPPUTEST cpputest>=3.7)
+pkg_check_modules(PC_CPPUTEST cpputest QUIET)
 
 find_path(CPPUTEST_INCLUDE_DIR CppUTest/CppUTestConfig.h
   HINTS ${PC_CPPUTEST_INCLUDEDIR} ${PC_CPPUTEST_INCLUDE_DIRS}
+  ${CPPUTEST_DIR}/include
 )
 
 find_library(CPPUTEST_LIBRARY NAMES CppUTest libCppUTest
   HINTS ${PC_CPPUTEST_LIBDIR} ${PC_CPPUTEST_LIBRARY_DIRS}
+  ${CPPUTEST_DIR}/lib ${CPPUTEST_DIR}/lib64
 )
 
 include(FindPackageHandleStandardArgs)
