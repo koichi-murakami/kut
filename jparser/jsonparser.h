c952bc657bab81e2593e659e74eedc9001f6849c
Koichi Murakami <koichi.murakami@kek.jp>
2021-02-26 20:16:03 +0900

    add GetFloatValue() method, update c++11 style

--- jparser/jsonparser.h
+++ jparser/jsonparser.h
@@ -1,5 +1,5 @@
 /*============================================================================
-  Copyright 2017 Koichi Murakami
+  Copyright 2017-2021 Koichi Murakami
 
   Distributed under the OSI-approved BSD License (the "License");
   see accompanying file License for details.
@@ -15,12 +15,6 @@
 #include <vector>
 #include "picojson.h"
 
-#ifndef DISALLOW_COPY_AND_ASSIGN
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete;      \
-  void operator=(const TypeName&) = delete
-#endif
-
 namespace kut {
 
 class JsonParser {
@@ -28,11 +22,12 @@ public:
   typedef std::vector<bool> barray_t;
   typedef std::vector<int> iarray_t;
   typedef std::vector<long> larray_t;
+  typedef std::vector<float> farray_t;
   typedef std::vector<double> darray_t;
   typedef std::vector<std::string> sarray_t;
 
   static JsonParser* GetJsonParser();
-  ~JsonParser();
+  ~JsonParser() = default;
 
   bool LoadFile(const std::string& fname);
 
@@ -41,20 +36,23 @@ public:
   bool GetBoolValue(const char* key) const;
   int GetIntValue(const char* key) const;
   long GetLongValue(const char* key) const;
+  float GetFloatValue(const char* key) const;
   double GetDoubleValue(const char* key) const;
   std::string GetStringValue(const char* key) const;
 
   std::size_t GetBoolArray(const char* key, barray_t& barray) const;
   std::size_t GetIntArray(const char* key, iarray_t& iarray) const;
   std::size_t GetLongArray(const char* key, larray_t& larray) const;
+  std::size_t GetFloatArray(const char* key, farray_t& farray) const;
   std::size_t GetDoubleArray(const char* key, darray_t& darray) const;
   std::size_t GetStringArray(const char* key, sarray_t& sarray) const;
 
   void DumpAll() const;
 
 private:
-  JsonParser();
-  DISALLOW_COPY_AND_ASSIGN(JsonParser);
+  JsonParser() = default;
+  JsonParser(const JsonParser&) = delete;    
+  void operator=(const JsonParser&) = delete;
 
   picojson::value data_map_;
 
